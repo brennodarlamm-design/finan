7ef0f7e6a9bc9e114360a5be7c26d066922e0ea1
brennodarlamm-design <brennodarlamm@gmail.com>
2026-09-12 21:57:30 -0400

    test(patch34): include deep-link regression

--- scripts/test-static-all.js
+++ scripts/test-static-all.js
@@ -35,7 +35,8 @@ const tests = [
   'scripts/test-patch30-static.js',
   'scripts/test-patch31-static.js',
   'scripts/test-patch32-static.js',
-  'scripts/test-patch33-static.js'
+  'scripts/test-patch33-static.js',
+  'scripts/test-patch34-static.js'
 ].filter(fs.existsSync);
 for (const file of tests) {
   console.log(`\n=== ${file} ===`);
