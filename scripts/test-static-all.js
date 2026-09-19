2b70f2375ae5d7009e5b14f0edd8d57b085aabcf
brennodarlamm-design <brennodarlamm@gmail.com>
2026-09-19 13:26:36 -0400

    test(bim): include clash engine regression

--- scripts/test-static-all.js
+++ scripts/test-static-all.js
@@ -96,6 +96,7 @@ const tests = [
   'scripts/test-evolutionary-pillars.js',
   'scripts/test-bim-viewer-work-pr.js',
   'scripts/test-bim-geometry-importer.js',
+  'scripts/test-bim-clash-engine.js',
   'scripts/test-3d-skills.js'
 ].filter(fs.existsSync);
 
